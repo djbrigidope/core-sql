6e4cdb3912b5398b1e537d6ef0bc062ee9ccfba1
Monica Williams <monicaestellaw@gmail.com>
2017-03-19 16:26:34 -0700

    adds student table to schema

---
+++ sql/01-create-table-student.sql
@@ -0,0 +1,8 @@
+/* **01:** Table name: `student`, columns: `id`, `name`, `grade`
+- `id` is the primary key
+- `name` is only their first name
+- `grade` is an integer
+- write the SQL in the file `sql/01-create-table-student.sql`
+*/
+
+CREATE TABLE student ( id SERIAL PRIMARY KEY, name VARCHAR(255), grade INTEGER );
